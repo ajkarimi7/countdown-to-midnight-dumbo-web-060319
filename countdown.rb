2e036edbe0bb8015d4d36a3e10939e2579b2c4d4
Ali Karimi <ajkarimi7@gmail.com>
2019-05-31 03:02:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,7 +11,7 @@ end
 
 def countdown_with_sleep(seconds_to_midnight)
   while seconds_to_midnight > 0 
-    puts "#{seconds_to_midnight} SECOND(S)!"
+    puts "#{seconds_to_midnight} SECONDS(S)!"
     sleep(1)
     seconds_to_midnight -= 1
   end
